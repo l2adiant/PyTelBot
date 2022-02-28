e586f6f52667435c24937a3ff663c10792001575
l2adiant <l2adiant@gmail.com>
2022-02-28 23:22:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ PORT = int(os.environ.get('PORT', '8443'))
 from telegram import Update, ForceReply
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
-TOKEN = os.environ['API_kEY']
+TOKEN = os.environ['API_KEY']
 # Enable logging
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
